7df1403421662a4725e13ae3429833543eb2d7ae
Johann Dolivet <jdolivet@hotmail.com>
2019-03-13 15:12:15 -0300

    Update and rename 4-2-fonction.py to 4-2-precision.py

---
+++ Cours 2019/4-2-precision.py
@@ -0,0 +1,18 @@
+"""
+Created on 03-13-2019
+
+@author: Johann Dolivet
+
+Précision des calculs sur les flottants
+
+Certains modules permettent de nous aider à gérer les problèmes de précisions
+"""
+
+
+from decimal import Decimal
+
+x = Decimal('0.0')
+nbIter = 3
+for i in range(nbIter):
+    x = x + Decimal('0.1')
+print(nbIter, "* 0.1 =", x)
