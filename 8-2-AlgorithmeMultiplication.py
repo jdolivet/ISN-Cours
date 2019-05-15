26a8fc1079b9f293779b4567064d9cc20de7ef12
Johann Dolivet <jdolivet@hotmail.com>
2019-05-15 15:22:09 -0300

    Séance du 15/05
    Algorithmique
    Terminaison et validité (algorithme de multiplication)
    Calcul du PGCD (recherche exhaustive, algorithme des soustractions, algorithme d'Euclide)

---
+++ 8-2-AlgorithmeMultiplication.py
@@ -0,0 +1,20 @@
+"""
+Created on 05-15-2019
+
+@author: Johann Dolivet
+
+Correction d'un algorithme
+Calcul de (a * b) en n'effectuant que des multiplications et divisions par 2.
+Invariant de boucle : z + x * y = a * b
+"""
+
+x = int(input("Entrez un entier a : "))
+y = int(input("Entrez un entier b : "))
+z = 0
+invariant = z + x * y
+while y != 0:
+    if y % 2 == 1:
+        z = z + x
+    x = 2 * x
+    y = y // 2
+print("Le résultat est : ", z)
