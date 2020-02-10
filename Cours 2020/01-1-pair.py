f0797382fff0e93ed32e1b9537744d1acb8bd8e6
Johann Dolivet <jdolivet@hotmail.com>
2020-02-10 19:51:18 -0300

    Séance du 10/02
    - Présentation
    - Bases
    - Instruction conditionnelle

---
+++ Cours 2020/01-1-pair.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 02-10-2020
+
+@author: Johann Dolivet
+
+Instruction conditionnelle
+Ce script permet de déterminer si le nombre x est pair ou non
+"""
+
+x = 123453
+
+if x % 2 == 0:
+    print("pair")
+else:
+    print("impair")
+print("Done with conditional")
