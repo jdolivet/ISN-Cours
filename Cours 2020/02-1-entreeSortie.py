27a81159a5138f92b231b189a7c1b00eb4e21ae7
Johann Dolivet <jdolivet@hotmail.com>
2020-02-17 17:57:08 -0300

    Seance du 17/02/2020
    Chaine de caracteres
    Boucle while
    Methode exhaustive
    Boucle for

---
+++ Cours 2020/02-1-entreeSortie.py
@@ -0,0 +1,12 @@
+"""
+Created on 02-17-2020
+
+@author: Johann Dolivet
+
+Entrée - Sortie
+Ce script permet à l'utilisateur d'entrer des valeurs
+"""
+
+ent = int(input("Entrez un nombre entier : "))
+ent = ent + 1
+print("Le nombre suivant est :", ent)
---
+++ Cours 2020/02-3-exhaustivite.py
@@ -0,0 +1,20 @@
+"""
+Created on 02-17-2020
+
+@author: Johann Dolivet
+
+Exhaustivité (check and guess)
+Ce script utilise une boucle while
+Permet de calculer la racine cubique d'un cube parfait
+"""
+
+x = int(input("Entrez un nombre entier : "))
+ans = 0
+while ans ** 3 < abs(x):
+    ans = ans + 1
+if ans ** 3 != abs(x):
+    print(x,"n'est pas un cube parfait")
+else:
+    if x < 0:
+        ans = - ans
+    print("La racine cubique de", x, "est", ans)
